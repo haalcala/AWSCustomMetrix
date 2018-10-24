fe57c0c5bea5d29e056043ba95db40364f4456ab
Harold Alcala <harold.alcala@vcube.co.jp>
2018-10-24 15:27:20 +0800

    Installer

--- installer_src.sh
+++ installer_src.sh
@@ -4,72 +4,19 @@ echo ""
 echo "AWS Custom Metrix Installer"
 echo ""
 
-if [ "$USER" != "root" ]; then
-    echo "This script is required to be run as root."
-    exit 1
-fi
+# Create destination folder
+DESTINATION="$PWD/aws-scripts"
+mkdir -p ${DESTINATION}
 
-instanceId=$(curl -m 3 -s 'http://169.254.169.254/latest/meta-data/instance-id')
+echo "Extracting archive ..."
 
-if [ "$instanceId" == "" ]; then
-    echo "Unable to check instanceId.  Please note that this script only works directly on an AWS EC2 instance"
-    exit 1
-fi
+# Find __ARCHIVE__ maker, read archive content and decompress it
+ARCHIVE=$(awk '/^__ARCHIVE__/ {print NR + 1; exit 0; }' "${0}")
+tail -n+${ARCHIVE} "${0}" | tar xvz -C ${DESTINATION}
 
-if [ ! -d "./aws-scripts" ]; then
-    # Create destination folder
-    DESTINATION="$PWD/aws-scripts"
-    mkdir -p ${DESTINATION}
+echo "Extracting archive ... done."
 
-    echo "Extracting archive ..."
-
-    # Find __ARCHIVE__ maker, read archive content and decompress it
-    ARCHIVE=$(awk '/^__ARCHIVE__/ {print NR + 1; exit 0; }' "${0}")
-    tail -n+${ARCHIVE} "${0}" | tar xvz -C ${DESTINATION}
-
-    echo "Extracting archive ... done."
-fi
-
-if [ -e "./aws-scripts/cloudwatch/.credential" ]; then
-    . ./aws-scripts/cloudwatch/.credential
-fi
-
-if [ "$AWSAccessKeyId" == "" ] || [ "$AWSSecretKey" == "" ]; then
-    echo "Some AWS credentials are found missing. These are required."
-    
-    while [ "$AWSAccessKeyId" == "" ]; do read -p 'AWSAccessKeyId: ' AWSAccessKeyId ; done
-    while [ "$AWSSecretKey" == "" ]; do read -p 'AWSSecretKey: ' AWSSecretKey ; done
-
-    [ ! -e "./aws-scripts/cloudwatch" ] && mkdir -p ./aws-scripts/cloudwatch
-
-    echo "AWSAccessKeyId=$AWSAccessKeyId" > ./aws-scripts/cloudwatch/.credential
-    echo "AWSSecretKey=$AWSSecretKey" >> ./aws-scripts/cloudwatch/.credential
-fi
-
-if [ ! -e "./aws-scripts/cloudwatch/.ec2_region" ]; then
-    echo "Please provide ec2-region for this cloudwatch."
-
-    while [ "$EC2_REGION" == "" ]; do read -p 'EC2_REGION: ' EC2_REGION ; done
-
-    custom_metrics_script=$(cat ./aws-scripts/cloudwatch/custom_metrics_orig.sh)
-    custom_metrics_script=$(echo "$custom_metrics_script" | sed -e "s/__EC2_REGION__/$EC2_REGION/g")
-
-    echo "$custom_metrics_script" > ./aws-scripts/cloudwatch/custom_metrics.sh
-
-    chmod u+x aws-scripts/cloudwatch/custom_metrics.sh
-
-    echo "$EC2_REGION" > ./aws-scripts/cloudwatch/.ec2_region
-fi
-
-if [ "$(crontab -l | grep custom_metrics)" == "" ]; then
-    echo "Cron job not found."
-    echo "Scheduling cron job ..."
-
-    cronjob="*/5 * * * * $PWD/aws-scripts/cloudwatch/custom_metrics.sh"
-    (crontab -l; echo "$cronjob" ) | crontab -
-
-    echo "Scheduling cron job ... done."
-fi
+sudo ./aws-scripts/install.sh
 
 echo ""
 echo "Installation complete."
